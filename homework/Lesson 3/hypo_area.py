2f33f77f820b89176b9293fd28ce1a5bfc94147a
Sam3081 <malary0@gmail.com>
2024-04-10 15:48:22 +0300

    Исправление возможной ошибки

--- homework/Lesson 3/hypo_area.py
+++ homework/Lesson 3/hypo_area.py
@@ -2,6 +2,6 @@ first_cat = float(input('First cathetus = '))
 second_cat = float(input('Second cathetus = '))
 
 hypotenuse = (first_cat ** 2 + second_cat ** 2) ** 0.5
-area = (first_cat * second_cat) /
+area = (first_cat * second_cat) / 2
 
 print(float(hypotenuse), float(area), sep='\n')
