ea7b294de8ac822c939d50d4bddc846d43e32308
Sam3081 <malary0@gmail.com>
2024-04-10 14:56:07 +0300

    calc - вычисление суммы, разницы, произведения. exp - вычисление выражения, данного в задаче. hypo_area - вычисление гипотенузы и площади. mean - вычисление средне арифметического и средне геометрического чисел

---
+++ homework/Lesson 3/mean.py
@@ -0,0 +1,7 @@
+first_int = float(input('First number = '))
+second_int = float(input('Second number = '))
+
+mean_arif = (first_int + second_int) / 2
+mean_geom = (first_int * second_int) ** 0.5
+
+print(float(mean_arif), float(mean_geom), sep='\n')
